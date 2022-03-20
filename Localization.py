888d1cfa2a1eb0e61f2e8d9b7b2cd50c1a3ec3d8
Joris Rijsdijk <j.a.rijsdijk@student.tudelft.nl>
2022-03-20 18:02:06 +0100

    Add simple screen localization Add setup to load and go through videos at a certain sampling rate

---
+++ Localization.py
@@ -0,0 +1,137 @@
+import math
+import random
+from collections import namedtuple
+import cv2
+import numpy as np
+
+Box = namedtuple('Box', ["minX", "maxX", "minY", "maxY"])
+
+
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+def getCropBox(image):
+    w = image.shape[0]
+    h = image.shape[1]
+    minX = w
+    maxX = 0
+    minY = h
+    maxY = 0
+    for x in range(0, w):
+        for y in range(0, h):
+            if image[x, y] > 0:
+                minX = min(minX, x)
+                maxX = max(maxX, x)
+                minY = min(minY, y)
+                maxY = max(maxY, y)
+    maxX += 1
+    maxY += 1
+    return Box(minX, maxX, minY, maxY)
+
+
+def crop(image, box: Box):
+    return image[box.minX:box.maxX, box.minY:box.maxY]
+
+
+def contourToBox(contour, inflate=0):
+    minX = float('inf')
+    maxX = 0
+    minY = float('inf')
+    maxY = 0
+    for C in contour:
+        c = C[0]
+        x = c[1]
+        y = c[0]
+        minX = min(minX, x)
+        maxX = max(maxX, x)
+        minY = min(minY, y)
+        maxY = max(maxY, y)
+    return Box(minX-inflate, maxX+inflate, minY-inflate, maxY+inflate)
+
+
+def opening(img, sl):
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, sl)
+
+
+def closing(img, sl):
+    return cv2.morphologyEx(img, cv2.MORPH_CLOSE, sl)
+
+
+def rect(x, y):
+    return cv2.getStructuringElement(cv2.MORPH_RECT, (x, y))
+
+
+def square(s):
+    return rect(s, s)
+
+
+def isInvalidImage(image):
+    if image is None or image.shape[0] < 10 or image.shape[1] < 30:
+        return True
+    return False
+
+def binarize(screen):
+    screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    screen = cv2.adaptiveThreshold(screen, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 8)
+    screen = opening(screen, square(5))
+    return screen
+
+# we might wanna include the brightness of the screen
+def localize(screen, display_steps=False):
+    mask = cv2.bitwise_not(binarize(screen))
+    contours, h = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    cs = []
+    for c in contours:
+        b = contourToBox(c, 0)
+        widthRatio = (b[3] - b[2] + 1) / (b[1] - b[0] + 1)
+
+        if (b[3] - b[2]) * (b[1] - b[0]) < (screen.shape[0] * screen.shape[1]) / 8 or not (1.4 < widthRatio < 1.8 or 1.4 < 1 / widthRatio < 1.8):
+            continue
+        cs.append(c)
+        s = crop(screen, b)
+        return s
+
+    return None
+
+
+# # get rotation angle from a cropped plate so it would be axis aligned.
+# def rotateScreen(screen):
+#     if isInvalidImage(screen):
+#         return None
+#     edges = cv2.Canny(screen, 50, 200, None, 3)
+#     lines = cv2.HoughLines(edges, 1, np.pi / 180, 20, None, 0, 0)
+#
+#     # get first line
+#     if lines is None:
+#         return screen
+#
+#     rho = lines[0][0][0]
+#     theta = lines[0][0][1]
+#     a = math.cos(theta)
+#     b = math.sin(theta)
+#     x0 = a * rho
+#     y0 = b * rho
+#     pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
+#     pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
+#     slope = (pt2[1] - pt1[1]) / max((pt2[0] - pt1[0]), 1)
+#
+#     angle = np.rad2deg(np.arctan(slope))
+#     r_screen = rotate_image(screen, angle)
+#
+#     return localize(r_screen)
+
+
+def screen_detection(image, display_steps=False):
+    # localize
+    screen = localize(image, display_steps)
+
+    if display_steps and screen is not None:
+        cv2.imshow("localized screen", screen)
+        cv2.waitKey(0)
+
+    return screen
--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 #!/bin/env python
+import os
 
 import cv2
 import numpy as np
 
-noise = np.random.rand(600, 300) * 255
-
-cv2.imshow("Noise", noise)
-cv2.waitKey()
+import Localization
 
 
+def processFrame(frame, display_steps=True):
+    localizedFrame = Localization.screen_detection(frame, display_steps)
+
+
+def processVideo(video, sample_frequency=1):
+    frame_number = 0
+    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    frames_per_sample = fps // sample_frequency
+
+    while video.isOpened and frame_number < frame_count:
+        ret, frame = video.read()
+        if frame_number % frames_per_sample == 0:
+            processFrame(frame)
+        frame_number += 1
+
+
+def getVideo(index=0):
+    path = "Input Videos/" + os.listdir("Input Videos")[index]
+    return cv2.VideoCapture(path)
+
+def main():
+    video = getVideo()
+    processVideo(video)
+
+
+if __name__ == "__main__":
+    main()
