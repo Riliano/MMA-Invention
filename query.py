d94d728b74218c61039f507a7e9ab88c048db45c
Lazar Nenovski <lazar.nenovski@gmail.com>
2022-03-21 19:45:26 +0000

    Added argument parser and renamed the two main files

--- query.py
+++ query.py
@@ -1,10 +1,18 @@
 #!/bin/env python
 import os
+import argparse
+import pathlib
 
 import cv2
 import numpy as np
 
 import Localization
+
+parser = argparse.ArgumentParser(description='Query tool for the video database created by <TODO>. Returns a filename for the best match.')
+
+default = "Input Videos/" + os.listdir("Input Videos")[0], #temp
+parser.add_argument('query', type=pathlib.Path, help='The path to the query video.')
+
 
 
 def processFrame(frame, display_steps=True):
@@ -17,7 +25,8 @@ def processVideo(video, sample_frequency=1):
     fps = int(video.get(cv2.CAP_PROP_FPS))
     frames_per_sample = fps // sample_frequency
 
-    while video.isOpened and frame_number < frame_count:
+    print('ble', video.isOpened())
+    while video.isOpened() and frame_number < frame_count:
         ret, frame = video.read()
         if frame_number % frames_per_sample == 0:
             processFrame(frame)
@@ -26,10 +35,20 @@ def processVideo(video, sample_frequency=1):
 
 def getVideo(index=0):
     path = "Input Videos/" + os.listdir("Input Videos")[index]
+    print(path)
     return cv2.VideoCapture(path)
 
 def main():
-    video = getVideo()
+    args = parser.parse_args()
+    path = args.query.resolve()
+
+    video = cv2.VideoCapture(str(path.resolve()))
+
+    if (video.isOpened()== False):
+        print("Error opening video stream or file. Does the path exist?")
+
+    print('Results for: ' + path.name)
+    #video = getVideo()
     processVideo(video)
 
 
